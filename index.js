6a210da564184b122809ff5b16f477e7c4e152c4
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2020-10-14 15:01:57 -0600

    Completed Project MVP, next going to work on Stretch

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
 
 /*
   TASK 2
@@ -57,8 +72,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
 }
 
 /*
@@ -68,18 +90,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. There is Global binding, which is the default for "this," usually refers to the JavaScript Window
+  2. There is Implicit binding, which is when "this" refers to the object that contains the function. Look to the left of the dot.
+  3. There is explicit binding, which is when you use .apply, .call, or .bind. It tells JavaScript where to look for "this".
+  4. Lastly, there is new binding, which is when you use a constructor function. In this case, "this" refers to the new object created.
 */
 
 
